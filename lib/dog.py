8e6025ef9bf184e09e3c9529ab6315065d021710
Victor <wanjala94vic@gmail.com>
2023-09-16 20:40:20 +0300

    completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self,name,breed='Mutt'):
+        self.name = name
+        self.breed = breed
+Fido = Dog('German','Caleb')
+print(f'{Fido.name}')
+print(f'{Fido.breed}')
+
+    
+       
+pass
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+    def __init__(self,name):
+        self.name = name
+person_name = Person('Victor')
+print(person_name.name)
+
+ 
+pass
